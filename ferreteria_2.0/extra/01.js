6f6979041ebdfd9eceee8455e57f3e0a34ce81b7
JavierP <javier.poblete2023@alu.uct.cl>
2023-09-30 02:38:07 -0300

    agregada primera parte de buscador

--- ferreteria_2.0/extra/01.js
+++ ferreteria_2.0/extra/01.js
@@ -10,3 +10,24 @@ document.addEventListener('click', e => {
         $signUp.classList.toggle('active')
     }
 });
+
+document.getElementById("icon-search").addEventListener("click", mostrar_buscador);
+document.getElementById("cover-ctn-search").addEventListener("click", ocultar_buscador);
+
+bars_search = document.getElementById("ctn-bars-search");
+cover_ctn_search = document.getElementById("cover-ctn-search");
+inputSearch = document.getElementById("inputSearch");
+box_search = document.getElementById("box-search");
+
+function mostrar_buscador(){
+    bars_search.style.top = "80px";
+    cover_ctn_search.style.display = "block";
+    inputSearch.focus();
+}
+
+function ocultar_buscador(){
+
+    bars_search.style.top = "-10px";
+    cover_ctn_search.style.display = "none";
+    inputSearch.value = "";
+}
