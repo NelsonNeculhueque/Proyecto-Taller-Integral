3ba1ea592789ea5ecb2cd59e69671b8339926f31
JavierP <javier.poblete2023@alu.uct.cl>
2023-09-30 13:57:24 -0300

    buscador final (no busca los productos)

--- ferreteria_2.0/extra/01.js
+++ ferreteria_2.0/extra/01.js
@@ -30,4 +30,26 @@ function ocultar_buscador(){
     bars_search.style.top = "-10px";
     cover_ctn_search.style.display = "none";
     inputSearch.value = "";
+    box_search.style.display = "none"
+}
+
+
+document.getElementById("inputSearch").addEventListener("keyup", buscador_interno)
+function buscador_interno(){
+
+    filter = inputSearch.value.toUpperCase();
+    li = box_search.getElementsByTagName("li");
+
+    for (i = 0; i < li.length; i++){
+        a = li[i].getElementsByTagName("a")[0];
+        textValue = a.textContent || a.innerText;
+
+        if(textValue.toUpperCase().indexOf(filter) > -1){
+
+            li[i].style.display = "";
+            box_search.style.display = "block";
+        }else{
+            li[i].style.display = "none";
+        }
+    }
 }
