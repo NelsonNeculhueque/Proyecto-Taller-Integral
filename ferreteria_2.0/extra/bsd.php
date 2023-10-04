fa22d76e407384f001c316a6c6d3e1cb4cbd76df
JavierP <javier.poblete2023@alu.uct.cl>
2023-10-03 22:36:20 -0300

    detallito arreglado

--- ferreteria_2.0/extra/bsd.php
+++ ferreteria_2.0/extra/bsd.php
@@ -1,12 +1,11 @@
 <?php
-// Conéctate a la base de datos
-$conexion = mysqli_connect("localhost", "nelson", "Nelson_2005", "FerreteriaVirtual");
+
+$conexion = mysqli_connect("localhost", "root", "", "ferreteriavirtual");
 
 if (!$conexion) {
     die("La conexión falló: " . mysqli_connect_error());
 }
 
-// Consulta SQL para obtener los nombres de los productos de 'maquinariamotor'
 $consulta_maquinariamotor = "SELECT Tipo_Maquinaria, Modelo FROM maquinariamotor";
 $resultado_maquinariamotor = mysqli_query($conexion, $consulta_maquinariamotor);
 
@@ -14,7 +13,6 @@ if (!$resultado_maquinariamotor) {
     die("La consulta de 'maquinariamotor' falló: " . mysqli_error($conexion));
 }
 
-// Consulta SQL para obtener los nombres de los productos de 'suplementos'
 $consulta_suplementos = "SELECT Nombre_Suple FROM suplementos";
 $resultado_suplementos = mysqli_query($conexion, $consulta_suplementos);
 
@@ -22,7 +20,7 @@ if (!$resultado_suplementos) {
     die("La consulta de 'suplementos' falló: " . mysqli_error($conexion));
 }
 
-// Consulta SQL para obtener los nombres de los productos de 'implementoseg'
+
 $consulta_implementoseg = "SELECT Tipo_Implemento FROM implementoseg";
 $resultado_implementoseg = mysqli_query($conexion, $consulta_implementoseg);
 
@@ -30,7 +28,6 @@ if (!$resultado_implementoseg) {
     die("La consulta de 'implementoseg' falló: " . mysqli_error($conexion));
 }
 
-// Crear un arreglo para almacenar todos los nombres de productos
 $nombres_productos = array();
 
 while ($fila = mysqli_fetch_assoc($resultado_maquinariamotor)) {
@@ -47,10 +44,9 @@ while ($fila = mysqli_fetch_assoc($resultado_implementoseg)) {
     $nombres_productos[] = $fila['Tipo_Implemento'];
 }
 
-// Cierra la conexión a la base de datos
+
 mysqli_close($conexion);
 
-// Imprime los nombres de los productos como una respuesta JSON
 header('Content-Type: application/json');
 echo json_encode($nombres_productos);
 ?>
